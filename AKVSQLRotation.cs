2bebb05c0412a96b26ee711074597085004d5575
Dan Friedman <dan@thefriedmans.org>
2021-01-27 10:56:07 -0600

    Regex

--- AKVSQLRotation.cs
+++ AKVSQLRotation.cs
@@ -18,7 +18,7 @@ namespace Microsoft.KeyVault
         {
             log.LogInformation("C# Event trigger function processed a request.");
             var secretName = eventGridEvent.Subject;
-            var secretVersion = Regex.Match(eventGridEvent.Data.ToString(), "Version\":\"([a-z0-9]*)").Groups[1].ToString();
+            var secretVersion = Regex.Match(eventGridEvent.Data.ToString(), "Version\":\s*\"([a-z0-9]*)").Groups[1].ToString();
             var keyVaultName = Regex.Match(eventGridEvent.Topic, ".vaults.(.*)").Groups[1].ToString();
             log.LogInformation($"Key Vault Name: {keyVaultName}");
             log.LogInformation($"Secret Name: {secretName}");
