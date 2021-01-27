99c05cadf128f32965b0bea0a4a28fe0db6881e6
Dan Friedman <dan@thefriedmans.org>
2021-01-27 11:59:51 -0600

    Fix regex

--- AKVSQLRotation.cs
+++ AKVSQLRotation.cs
@@ -7,6 +7,7 @@ using Microsoft.Azure.EventGrid.Models;
 using Microsoft.Azure.WebJobs.Extensions.EventGrid;
 using Microsoft.Azure.WebJobs;
 using System.Text.RegularExpressions;
+using Newtonsoft.Json;
 
 namespace Microsoft.KeyVault
 {
@@ -18,13 +19,13 @@ namespace Microsoft.KeyVault
         {
             log.LogInformation("C# Event trigger function processed a request.");
             var secretName = eventGridEvent.Subject;
-            var secretVersion = Regex.Match(eventGridEvent.Data.ToString(), "Version\":\s*\"([a-z0-9]*)").Groups[1].ToString();
+            var secretVersion = Regex.Match(eventGridEvent.Data.ToString(), "Version\":\\s*\"([a-z0-9]*)").Groups[1].ToString();
             var keyVaultName = Regex.Match(eventGridEvent.Topic, ".vaults.(.*)").Groups[1].ToString();
             log.LogInformation($"Key Vault Name: {keyVaultName}");
             log.LogInformation($"Secret Name: {secretName}");
             log.LogInformation($"Secret Version: {secretVersion}");
-            log.LogInformation($"Event Data: {eventGridEvent.Data.ToString()}");
-            log.LogInformation($"Event: {eventGridEvent.ToString()}");
+            log.LogInformation($"Event: {JsonConvert.SerializeObject(eventGridEvent)}");
+            log.LogInformation($"Event Data: {eventGridEvent.Data}");
 
             SecretRotator.RotateSecret(log, secretName, keyVaultName);
         }
